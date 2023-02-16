da5cc5463df3eb548e490988b9579e60f8405f44
Derga <dergan73@mail.ru>
2023-02-16 20:40:21 +0400

    пофиксил баги, работает смена темы

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar";
-
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./scenes/dashboard";
 
 const App = () => {
 
@@ -9,14 +10,22 @@ const App = () => {
 
   return (
     <ColorModeContext.Provider value={colorMode}>
+
       <ThemeProvider theme={theme}>
+
         <CssBaseline />
         <div className="app">
+
           <main className="content">
             <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
           </main>
+
         </div>
       </ThemeProvider>
+
     </ColorModeContext.Provider>
   );
 }
